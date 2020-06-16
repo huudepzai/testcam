e0f240235a163fd5a15d2f0190cb4670fc493b6b
huudepzai <dinhhuugt@gmail.com>
2020-06-16 15:40:41 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <!-- CSS -->
+<style>
+  #my_camera{
+   width: 320px;
+   height: 240px;
+   border: 1px solid black;
+  }
+  </style>
+  
+  <div id="my_camera"></div>
+  <input type=button value="Take Snapshot" onClick="take_snapshot()">
+   
+  <div id="results" ></div>
+   
+  <!-- Webcam.min.js -->
+  <script type="text/javascript" src="webcam.min.js"></script>
+  <!-- Configure a few settings and attach camera -->
+  <script language="JavaScript">
+   Webcam.set({
+    width: 320,
+    height: 240,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+   });
+   Webcam.attach( '#my_camera' );
+  
+  function take_snapshot() {
+   
+   // take snapshot and get image data
+   Webcam.snap( function(data_uri) {
+    // display results in page
+    document.getElementById('results').innerHTML = 
+    '<img src="'+data_uri+'"/>';
+    } );
+  
+    
+  }
+  </script>
+</body>
+</html>
